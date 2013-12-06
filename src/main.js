d33169b34710401039dd5cfb76b9dca8f53b2a4d
Bence <ben.kormos@gmail.com>
2013-12-06 18:07:58 -0500

    initial commit

---
+++ src/main.js
@@ -0,0 +1,33 @@
+/* global: Compoo, jquery */
+(function (global, Compoo, $) {
+  var compoopcv = {};
+  
+  var View = compoopcv.View = Compoo.extend({
+    
+  });
+  
+  var Controller = compoopcv.Controller = Compoo.extend({
+    
+  });
+  
+  var Presenter = compoopcv.Presenter = Compoo.extend({
+    
+  });
+  
+  var PresenterGroup = compoopcv.PresenterGroup = Presenter.extend({
+    
+  });
+  
+  var Block = compoopcv.Block = Compoo.extend({
+    
+  });
+  
+  if (global.compoopcv) {
+    compoopcv._compoopcv = global.compoopcv;
+  }
+  
+  global.compoopcv = compoopcv;
+  
+})(function () {
+  return this;
+}(), Compoo, jQuery);
